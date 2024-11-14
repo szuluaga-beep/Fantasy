3d2f2e3719b4d9508844116544e94676b83e800e
szuluaga-beep <steven000069@hotmail.com>
2024-11-14 14:09:12 -0500

    Adding ef core

--- Fantasy/Fantasy.Backend/Program.cs
+++ Fantasy/Fantasy.Backend/Program.cs
@@ -1,9 +1,13 @@
+using Fantasy.Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
 
