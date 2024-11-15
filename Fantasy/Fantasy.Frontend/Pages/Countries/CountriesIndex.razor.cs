c2c8e3c4a292ed6e73ce565201ca1e11222330ab
szuluaga-beep <steven000069@hotmail.com>
2024-11-14 19:08:45 -0500

    Countries List

---
+++ Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,20 @@
+using Fantasy.Frontend.Repositories;
+using Fantasy.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Fantasy.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+
+        private List<Country>? Countries { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var responseHttp = await Repository.GetAsync<List<Country>>("api/countries");
+
+            Countries = responseHttp.Response;
+        }
+    }
+}
